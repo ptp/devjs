032600220d4e3ee8c0a6575d5307702ba992912e
Sean Wilson <sean@trintondesigns.com>
2015-05-23 15:35:21 -0400

    Add auto-deployment

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ Website = function(req, res){
 };
 
 module.exports = exports = Website;
+
+var gith = require('gith').create(8081);
+var exec = require('child_process');
+
+gith({
+	repo: 'developjavascript/Website-Platform'
+}).on('all', function(payload) {
+	exec('./lib/hook/hook.sh' + payload.branch, function(err, stdout, stderr) {
+		if (err) return err;
+		//console.log(stdout);
+	});
+});
+
