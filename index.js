ab4c8b5465c7a0363791d1d8e7cbded124a9a3c6
Jamen Marz <jamen.marz@gmail.com>
2015-05-24 21:34:49 -0600

    Removed Gith

--- index.js
+++ index.js
@@ -5,20 +5,7 @@ Website = function(req, res){
   req.tools = tools.req;
   res.tools = tools.res;
 
-  lib.pages(req, res, tools.lib); 
+  lib.pages(req, res, tools.lib);
 };
 
 module.exports = exports = Website;
-
-var gith = require('gith').create(8081);
-var exec = require('child_process');
-
-gith({
-	repo: 'developjavascript/Website-Platform'
-}).on('all', function(payload) {
-	exec('./lib/hook/hook.sh' + payload.branch, function(err, stdout, stderr) {
-		if (err) return err;
-		//console.log(stdout);
-	});
-});
-
