d4b6b0f11ee4b8ccfec3e7360856fd860bc123f5
Jamen Marzonie <jamenmarz@gmail.com>
2016-02-28 16:26:55 -0700

    Fresh start

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import assert from 'assert';
-import devjs from '../lib';
-
-describe('devjs', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
-});
